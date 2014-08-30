c18611aa224b58e22b4f4a5fefeb0faa08dbdc5f
Stephen Parente <webmakersteve@gmail.com>
2014-08-30 00:09:23 -0400

    Callback now returns dps.

--- engine/addon.cc
+++ engine/addon.cc
@@ -3,23 +3,56 @@
 #include "simulationcraft.hpp"
 
 using namespace v8;
+using namespace std;
 
-Handle<Value> Add(const Arguments& args) {
+Handle<Value> Run(const Arguments& args) {
   HandleScope scope;
+
+  if (args.Length() < 1) {
+      return scope.Close(Undefined());
+  }
+
+  if (args[0]->IsObject()) {
+    Handle<Object> object = Handle<Object>::Cast(args[0]);
+    Handle<Value> armoryValue = object->Get(String::New("armory"));
+    // Value like us,illidan,john
+    String::AsciiValue ascii(armoryValue);
+
+  }
 
   sim_t sim;
 
   std::vector<std::string> array(1);
   array[0] = "armory=us,illidan,john";
-  sim_t_response* response = sim.returns( array );
-  delete response; // I guess should use RAII?
+  sim_t_response* response (sim.returns( array ));
+
+  double player_dps = response->simulator->active_player->collected_data.dps.mean();
+  Local<Number> dps = Number::New(player_dps);
+
+  Local<Object> returnObj = Object::New();
+  returnObj->Set(String::New("DPS"), dps);
+
+  if (args.Length() > 1) {
+
+    if (args[1]->IsFunction()) {
+      Handle<Function> fn = Handle<Function>::Cast(args[1]);
+      const unsigned argc = 1;
+      Local<Value> argv[argc] = { Local<Value>::New(returnObj) };
+      fn->Call(Context::GetCurrent()->Global(), argc, argv);
+
+    }
+
+  }
+
+  delete response; // I guess should use RAII? Otherwise if we get an exception
+  // We get a GIANT problem (huge amounts of leaked memory)
 
   return scope.Close(Undefined());
 }
 
 void Init(Handle<Object> exports) {
-  exports->Set(String::NewSymbol("add"),
-      FunctionTemplate::New(Add)->GetFunction());
+  exports->Set(String::NewSymbol("run"),
+      FunctionTemplate::New(Run)->GetFunction());
 }
 
 NODE_MODULE(simc, Init)
--- engine/test.js
+++ engine/test.js
@@ -1,3 +1,9 @@
 var simc = require('./build/Release/simc');
 
 console.log(simc);
+
+simc.run({
+  "armory": "us,tichondrius,donpain"
+}, function (dps) {
+  console.log(dps);
+});
